82521d9194407ffc05e9c30093516b0d7e1d7d2f
SAI KUMAR MISALA <saikumar@bluesapience.com>
2022-04-11 17:40:28 +0530

    opening page text tests

---
+++ test/widget_test.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quizzler/main.dart' as app;
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  group('Test the opening page text', () {
+    testWidgets('To test first question text', (WidgetTester tester) async {
+      // arrange
+      await tester.pumpWidget(app.Quizzler());
+
+      // act
+      final text = find.text('Some cats are actually allergic to humans');
+
+      //assert
+      expect(text, findsOneWidget);
+    });
+
+    testWidgets('To test true button text', (WidgetTester tester) async {
+      // arrange
+      await tester.pumpWidget(app.Quizzler());
+
+      // act
+      final text = find.text('True');
+
+      //assert
+      expect(text, findsOneWidget);
+    });
+
+    testWidgets('To test false button text', (WidgetTester tester) async {
+      // arrange
+      await tester.pumpWidget(app.Quizzler());
+
+      // act
+      final text = find.text('False');
+
+      //assert
+      expect(text, findsOneWidget);
+    });
+  });
+}
